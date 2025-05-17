1186de68738547c6c2a276b55831b3d08a8122c3
Ozan Sülükpınar <ozansulukpinar@hotmail.com>
2025-05-17 05:10:00 +0300

    Update round4.js

--- round4.js
+++ round4.js
@@ -198,10 +198,15 @@ saveBtn.addEventListener("click", async () => {
     get(progressRef).then(snap => {
       const current = snap.exists() ? snap.val() : 0;
       const nextRoundName = getNextRoundName(roundName);
-      await set(progressRef, nextRoundName).then(() => {
+      await set(progressRef, nextRoundName);
+      saveBtn.disabled = true;
+      window.location.href = "jury-dashboard.html";
+      /*
+      set(progressRef, nextRoundName).then(() => {
         saveBtn.disabled = true;
         window.location.href = "jury-dashboard.html";
       });
+      */
     });
   });
 
